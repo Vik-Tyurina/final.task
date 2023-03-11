634d0bf72c3c7eb3286136545c2d23df7ca94a12
Viktoriya <viktyurina79@gmai.com>
2023-03-11 21:04:49 +0700

    Создаем метод, который будет перебирать первый массив и записывать во второй

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] array1 = new string[5] {"1", "3", "три", "десять", "восемь"};
 string[] array2 = new string[array1.Length];
+
+void SortArray1(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
